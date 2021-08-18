d4cb618bc6bf4fd020cf474904f0e3cf06a87f80
Tom Perkins <twperkins.work@gmail.com>
2021-08-18 15:19:18 +0100

    backend fully working, now just need to do views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :create, :new]
+  resources :restaurants do
+    resources :reviews, only: [:create, :new]
+  end
 end
